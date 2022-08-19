e7eae63a00eeddd182229ee91ee38015775f3503
Shubham-0711 <90834170+shbhm-0711@users.noreply.github.com>
2022-08-19 23:58:20 +0530

    added blog url and render functionality

--- src/App.js
+++ src/App.js
@@ -5,11 +5,25 @@ import Blog from "./Blog";
 import About from "./About";
 
 function App() {
+
+  const blogs = [
+    {
+      id: 1,
+      title: "Blog_one",
+      body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt, quae aspernatur? Ab nesciunt explicabo non? Molestiae iusto perspiciatis aut laborum architecto quam illo magnam fugiat est cum officiis consequatur provident ea, tempora at quaerat. Debitis soluta dolores vero natus quia praesentium deleniti voluptates rem magni itaque quos, delectus dolore autem!",
+    },
+    // {
+    //   id: 2,
+    //   title: "Blog_two",
+    //   body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt, quae aspernatur? Ab nesciunt explicabo non? Molestiae iusto perspiciatis aut laborum architecto quam illo magnam fugiat est cum officiis consequatur provident ea, tempora at quaerat. Debitis soluta dolores vero natus quia praesentium deleniti voluptates rem magni itaque quos, delectus dolore autem!",
+    // },
+  ];
+
   return (
     <div>
       <Routes>
-        <Route path="/" exact element={<HomePage />}></Route>
-        <Route path="/blog/:id" element={<Blog />}></Route>
+        <Route path="/" exact element={<HomePage blogs={blogs} />}></Route>
+        <Route path="/blog/:id" element={<Blog blogs={blogs} />}></Route>
         <Route path="/about" element={<About />}></Route>
       </Routes>
     </div>
--- src/Blog.js
+++ src/Blog.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-const Blog = () => {
-  return <h1>Blog</h1>;
+const Blog = (props) => {
+  const { id } = useParams();
+  const [blog, setBlog] = useState(null);
+  useEffect(() => {
+    let blogn = props.blogs.find((blog) => blog.id === parseInt(id));
+    if (blogn) {
+      setBlog(blogn);
+    }
+  }, []);
+  return (
+    <>
+      <h1>{blog.title}</h1>
+      <p>{blog.body}</p>
+    </>
+  );
 };
 
 export default Blog;
