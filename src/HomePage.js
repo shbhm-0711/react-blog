10dd76b7fd2b25c7895371658b000ffa647ea4fe
Shubham-0711 <90834170+shbhm-0711@users.noreply.github.com>
2022-08-21 18:03:52 +0530

    demo authentication added

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,26 +1,28 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import Card from "./Card";
+import { useState } from "react";
 
 const HomePage = (props) => {
+  const [authenticated, setAuthenticated] = useState(true);
   return (
     <>
-      <main>
-        <nav>
-          <h2 className="text-3xl inline-block">Welcome to the homepage!</h2>
-          <Link
-            className="bg-amber-500 text-cyan-800 rounded-lg px-1 m-5 "
-            to="/about"
-          >
-            ....About Us
-          </Link>
-        </nav>
-        <div className="flex flex-wrap flex-row ">
-          {props.blogs.map((item) => {
-            return <Card className="" blogItem={item} key={item.id} />;
-          })}
-        </div>
-      </main>
+      <nav>
+        <h2 className="text-3xl inline-block">Welcome to the homepage!</h2>
+        <Link
+          className="bg-amber-500 text-cyan-800 rounded-lg px-1 m-5 "
+          to="/about"
+        >
+          ....About Us
+        </Link>
+        <button onClick={() => setAuthenticated((prev) => !prev)}>
+          {authenticated ? "Log out" : "Log in"}
+        </button>
+      </nav>
+      <div className="flex flex-wrap flex-row ">
+        {props.blogs.map((item) => {
+          return <Card className="" blogItem={item} key={item.id} />;
+        })}
+      </div>
     </>
   );
 };
