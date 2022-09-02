6d395d8b83381c854bd31269527b33f976c51f5a
Shubham-0711 <90834170+shbhm-0711@users.noreply.github.com>
2022-09-02 22:07:30 +0530

    modified backdrop modal

--- src/Pages/Components/LogoutModal.js
+++ src/Pages/Components/LogoutModal.js
@@ -7,25 +7,38 @@ export const BackDrop = (props) => {
       onClick={() => {
         props.setNullAuth(null);
       }}
-      className="z-40 flex justify-around align- absolute w-full h-screen bg-black backdrop-opacity-80"
-    >
-      <div className="z-50 w-[1000px] h-[150px] sm:h-[200px] md:h-[270px] lg:h-[300px] bg-white rounded-b-3xl ">
-        <h1 className="z-50 align-middle text-6xl sm:text-7xl md:text-8xl lg:text-9xl text-center text-sky-600 ">
-          Logged out successfully!!
-        </h1>
-      </div>
+      className="z-40 absolute w-full h-screen bg-black opacity-80"
+    ></div>
+  );
+};
+
+export const Modal = () => {
+  return (
+    <div className="z-50 mx-auto absolute top-0 w-screen h-[150px] sm:h-[200px] md:h-[270px] lg:h-auto lg:p-10 bg-white rounded-b-3xl ">
+      <h1 className="z-50 align-middle text-6xl sm:text-7xl md:text-8xl lg:text-9xl text-center text-sky-600 ">
+        Logged out successfully!!
+      </h1>
     </div>
+    // <div
+    //   className="z-50 m-auto absolute top-0 w-screen lg:w-[1000px] md:w-screen  h-[150px] sm:h-[200px] md:h-[270px] lg:h-[300px]
+    // left-[calc(50%-(w-screen/2))] lg:left-[50%-500px] bg-white rounded-b-3xl "
+    // >
+    //   <h1 className="z-50 align-middle text-6xl sm:text-7xl md:text-8xl lg:text-9xl text-center text-sky-600 ">
+    //     Logged out successfully!!
+    //   </h1>
+    // </div>
   );
 };
 
 const LogoutModal = (props) => {
   return (
-    <div>
+    <>
+      {createPortal(<Modal />, document.getElementById("rootModal"))}
       {createPortal(
-        <BackDrop setNullAuth={props.setNullAuth}></BackDrop>,
-        document.getElementById("rootModal")
+        <BackDrop setNullAuth={props.setNullAuth} />,
+        document.getElementById("rootBackdrop")
       )}
-    </div>
+    </>
   );
 };
 
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -22,6 +22,42 @@ const HomePage = () => {
         ullam impedit modi adipisci odit dolorum! Enim eaque numquam quas
         repudiandae temporibus repellat eveniet in blanditiis excepturi quos!
       </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+        voluptatem, rerum nulla explicabo nostrum aliquam ad corporis! Velit
+        natus asperiores consequatur quaerat itaque reprehenderit explicabo
+        atque praesentium ducimus soluta expedita neque tempora facere, dolorum
+        sint exercitationem dicta voluptatibus! Officia hic eos aspernatur illo
+        perspiciatis at nostrum tempora minus dolorum vero quae, sunt quas a
+        magnam exercitationem reiciendis unde modi, voluptatem rerum aut
+        doloribus? Porro atque mollitia ratione! Soluta ipsa, itaque earum dicta
+        repudiandae incidunt ex vero eum porro placeat nam blanditiis
+        perferendis ut voluptate necessitatibus perspiciatis autem
+        exercitationem omnis voluptates qui voluptatibus? Repellat magni nam
+        accusamus temporibus iste rem, dolorem eos dolores labore commodi,
+        mollitia atque perferendis dolor eligendi? Minima ipsum ea tempore
+        molestias natus aliquam ducimus eum quam ipsa, labore quis, veritatis
+        sint in placeat porro hic! Explicabo unde magni quasi omnis vitae
+        debitis impedit illum porro minus, veritatis, sunt error natus dolor
+        laboriosam veniam harum tempora nisi provident exercitationem dolore
+        quod eius nihil odio. At explicabo rerum totam accusantium officiis sunt
+        officia eligendi sed maxime repudiandae quia dolorem, minima dicta, ad
+        illum temporibus accusamus distinctio, beatae perspiciatis? Tempore fuga
+        cum cupiditate quidem nobis, assumenda quod fugit doloremque ad nihil
+        quibusdam explicabo vitae fugiat ipsa illum, sed eveniet accusamus
+        recusandae? Obcaecati molestias consequuntur temporibus quae aperiam
+        illo reiciendis unde neque necessitatibus animi id fugiat error tenetur
+        doloremque molestiae officia, accusantium itaque beatae dignissimos,
+        voluptas vel. Deleniti maxime facere hic a delectus repellat obcaecati
+        ad sed numquam dolorem expedita aliquam, accusantium illum autem sunt
+        distinctio illo quos modi laudantium, ea alias quasi incidunt
+        necessitatibus mollitia. Dolorem optio perferendis exercitationem
+        asperiores eveniet quod earum quos alias culpa enim. Ipsam ea id, eaque
+        exercitationem dolorum iste illum alias numquam similique officia
+        aliquam laborum ut facilis aperiam quia unde aliquid natus nobis
+        reiciendis labore necessitatibus? Odio blanditiis facilis eligendi, quam
+        amet dignissimos provident.
+      </p>
     </div>
   );
 };
